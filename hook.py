33e894a7a6cd93b1372d976d211bb7d8e8987e92
Tyrone Patterson <tvpatterson@wpi.edu>
2019-08-08 08:51:56 -0400

    Change certificate name. Find cert based on file location

--- hook.py
+++ hook.py
@@ -1,5 +1,5 @@
 import logging
-import filecmp
+from pathlib import Path
 from subprocess import Popen
 
 name = 'SSL'
@@ -10,10 +10,9 @@ address = None
 async def initialize(app, services):
     Popen(['haproxy', '-q', '-f', 'plugins/ssl/templates/haproxy.conf'])
     logging.debug('Serving at https://127.0.0.1:443')
-    config = open('plugins/ssl/templates/haproxy.conf', 'r').read()
-    path = config[config.index('crt') + 4:config.index('.pem') + 4]
-    if filecmp.cmp('plugins/ssl/conf/ssl_cert.pem', path):
-        logging.debug('Warning: Using insecure ssl private key and certificate. Consider generating your own to improve security. Documentation found here: https://github.com/mitre/caldera/wiki/Plugin:-ssl')
+    cert = Path("plugins/ssl/conf/insecure_certificate.pem")
+    if cert.is_file():
+        logging.warn('Found insecure ssl private key and certificate. Consider generating your own and removing the insecure certificate from the conf directory to improve security. Documentation found here: https://github.com/mitre/caldera/wiki/Plugin:-ssl')
 
 
 
