825c57551e4c5041e1983e14a1d8887b7e6f986b
Ravi B Serota <rserota@mitre.org>
2020-10-26 08:39:56 -0400

    Fixed logging address, added docs link to readme, changed hook.py to check insecure certificate used by the server

--- hook.py
+++ hook.py
@@ -1,10 +1,24 @@
 import logging
+import asyncio
 from pathlib import Path
 from subprocess import Popen
+from ssl import get_server_certificate
 
 name = 'SSL'
 description = 'Run an SSL proxy in front of the server'
-address = None
+address = ''
+
+async def _check_cert():
+    await asyncio.sleep(5)
+    default_cert = ''
+    server_cert = ''
+    server_cert = get_server_certificate(('127.0.0.1', 8443))
+    with open(Path('plugins/ssl/conf/insecure_certificate.pem'), 'r') as f:
+        default_cert = f.read()
+    if server_cert == default_cert:
+        logging.warn('Insecure SSL private key and certificate in use. Consider generating and using your own '
+                    'to improve security. Documentation found '
+                    'here: https://github.com/mitre/caldera/wiki/Plugin:-ssl')
 
 
 async def enable(services):
@@ -13,9 +27,6 @@ async def enable(services):
     if user_conf.is_file():
         haproxy_conf = user_conf
     Popen(['haproxy', '-q', '-f', haproxy_conf])
-    logging.debug('Serving at https://127.0.0.1:8443')
-    cert = Path("plugins/ssl/conf/insecure_certificate.pem")
-    if cert.is_file():
-        logging.warn('Found insecure SSL private key and certificate. Consider generating your own and removing'
-                     ' the insecure certificate from the conf directory to improve security. Documentation found '
-                     'here: https://github.com/mitre/caldera/wiki/Plugin:-ssl')
+    logging.debug('Serving at https://0.0.0.0:8443')
+    loop = asyncio.get_event_loop()
+    loop.create_task(_check_cert())
